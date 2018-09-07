9eed06a3830519c1c3742d0e1d9acdcb9a8c89af
Nguyen Anh Binh <sometimesocrazy@gmail.com>
2018-09-07 15:08:26 +0700

    Fisrt commit

--- PythonAPI/pycocotools/cocoeval.py
+++ PythonAPI/pycocotools/cocoeval.py
@@ -68,6 +68,7 @@ class COCOeval:
             print('iouType not specified. use default iouType segm')
         self.cocoGt   = cocoGt              # ground truth COCO API
         self.cocoDt   = cocoDt              # detections COCO API
+        self.params   = {}                  # evaluation parameters
         self.evalImgs = defaultdict(list)   # per-image per-category evaluation results [KxAxI] elements
         self.eval     = {}                  # accumulated evaluation results
         self._gts = defaultdict(list)       # gt for evaluation
@@ -266,7 +267,7 @@ class COCOeval:
         G = len(gt)
         D = len(dt)
         gtm  = np.zeros((T,G))
-        dtm  = np.zeros((T,D))
+        dtm  = np.zeros((T,D),object)
         gtIg = np.array([g['_ignore'] for g in gt])
         dtIg = np.zeros((T,D))
         if not len(ious)==0:
@@ -503,8 +504,8 @@ class Params:
         self.imgIds = []
         self.catIds = []
         # np.arange causes trouble.  the data point on arange is slightly larger than the true value
-        self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
-        self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
+        self.iouThrs = np.linspace(.5, 0.95, np.round((0.95 - .5) / .05) + 1, endpoint=True)
+        self.recThrs = np.linspace(.0, 1.00, np.round((1.00 - .0) / .01) + 1, endpoint=True)
         self.maxDets = [1, 10, 100]
         self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
         self.areaRngLbl = ['all', 'small', 'medium', 'large']
@@ -514,8 +515,8 @@ class Params:
         self.imgIds = []
         self.catIds = []
         # np.arange causes trouble.  the data point on arange is slightly larger than the true value
-        self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
-        self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
+        self.iouThrs = np.linspace(.5, 0.95, np.round((0.95 - .5) / .05) + 1, endpoint=True)
+        self.recThrs = np.linspace(.0, 1.00, np.round((1.00 - .0) / .01) + 1, endpoint=True)
         self.maxDets = [20]
         self.areaRng = [[0 ** 2, 1e5 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
         self.areaRngLbl = ['all', 'medium', 'large']
--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,27 +1,31 @@
-from setuptools import setup, Extension
+from distutils.core import setup
+from Cython.Build import cythonize
+from distutils.extension import Extension
 import numpy as np
+from setuptools import setup, Extension
 
-# To compile and install locally run "python setup.py build_ext --inplace"
-# To install library to Python site-packages run "python setup.py build_ext install"
+# To install and compile to your anaconda/python site-packages, simply run:
+# $ pip install git+https://github.com/philferriere/cocoapi.git#subdirectory=PythonAPI
+# Note that the original compile flags below are GCC flags unsupported by the Visual C++ 2015 build tools.
+# They can safely be removed.
 
 ext_modules = [
     Extension(
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
+        include_dirs=[np.get_include(), '../common'],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
-setup(
-    name='pycocotools',
-    packages=['pycocotools'],
-    package_dir = {'pycocotools': 'pycocotools'},
-    install_requires=[
-        'setuptools>=18.0',
-        'cython>=0.27.3',
-        'matplotlib>=2.1.0'
-    ],
-    version='2.0',
-    ext_modules= ext_modules
-)
+setup(name='pycocotools',
+      packages=['pycocotools'],
+      package_dir={'pycocotools': 'pycocotools'},
+      install_requires=[
+          'setuptools>=18.0',
+          'cython>=0.27.3',
+          'matplotlib>=2.1.0'
+      ],
+      version='2.0',
+      ext_modules=ext_modules
+      )
